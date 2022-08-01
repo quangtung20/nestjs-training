c8fa027689bed41b0af209e4c8e0d64799f26905
quangtung20 <tranquangtung20111999@gmail.com>
2022-08-01 19:01:01 +0700

    buoi4

--- practice3/src/controllers/user.controller.ts
+++ practice3/src/controllers/user.controller.ts
@@ -1,9 +1,56 @@
 import {Request,Response} from 'express';
+import { async } from 'rxjs';
 import { Users } from '../models/user.model';
 export const UserCtrl = {
     createUser:async(req:Request,res:Response)=>{
         const data = req.body;
         const newUser = await Users.create(data);
         res.json(newUser);
+    },
+
+    getAllUsers:async(req:Request,res:Response)=>{
+        const users = await Users.find();
+        res.status(200).json(users);
+    },
+
+    getUserById:async(req:Request,res:Response)=>{
+        const id = req.params.id;
+        const user = await Users.findOne({_id:id}).select("-password");
+        res.status(200).json(user);
+    },
+
+    getUserByEmail:async(req:Request,res:Response)=>{
+        const emailInput = req.query.email;
+        const user = await Users.findOne({email:emailInput}).select("-password");
+        res.status(200).json(user);
+    },
+
+    deleteUser:async(req:Request,res:Response)=>{
+        const id = req.params.id;
+        await Users.findByIdAndDelete(id);
+        res.json('delete successfully');
+    },
+
+    updateUser:async(req:Request,res:Response)=>{
+        const id = req.params.id;
+        const data = req.body;
+        const user = await Users.findOneAndUpdate({_id:id},{...req.body},{
+            new:true,
+        })
+
+        res.json(user);
+    },
+
+    pagination:async(req:Request,res:Response)=>{
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 5;  
+      console.log(page,limit);
+      const user = await Users.find().skip((page-1)*limit).limit(limit);
+      res.json(user);
     }
+
+
+
+    
+
 }
--- practice3/src/routes/user.route.ts
+++ practice3/src/routes/user.route.ts
@@ -4,5 +4,11 @@ import { UserCtrl } from '../controllers/user.controller';
 const router = express.Router(); // de dinh nghia cac duong dan den thu muc
 
 router.post('/',UserCtrl.createUser);
+router.get('/search',UserCtrl.getUserByEmail);
+router.get('/pagination',UserCtrl.pagination);
+router.get('/:id',UserCtrl.getUserById);
+router.get('/',UserCtrl.getAllUsers);
+router.delete('/:id',UserCtrl.deleteUser);
+router.put('/:id',UserCtrl.updateUser);
 
 export default router;
